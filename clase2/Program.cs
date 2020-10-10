37590b3b48657ad77398fe2d8716725538b8a55f
agusbarrio <agusbarrioeastman@gmail.com>
2020-10-10 19:40:39 -0300

    fin de la clase 2

--- clase2/Program.cs
+++ clase2/Program.cs
@@ -4,9 +4,29 @@ namespace clase2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // nuestro punto de inicio
         {
+            Console.WriteLine("Inicio del programa");
             Console.WriteLine("Hello World!");
+            saludarEspEng();
+            Console.WriteLine("Fin del programa");
+
+        }
+        
+        static void saludarEspEng()
+        {
+            saludarEsp("Hola soy argentino");
+            saludarEng();
+        }
+
+        static void saludarEsp(string mensaje)
+        {
+            Console.WriteLine(mensaje.ToUpper());   
+        }
+
+        static void saludarEng()
+        {
+            Console.WriteLine("welcome to the summer house");
         }
     }
 }
